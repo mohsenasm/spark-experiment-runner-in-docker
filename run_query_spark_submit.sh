d63854cfae3d40c035dc6a8beb8a2da68b02319e
giorgiopea <annatar93@gmail.com>
2016-11-25 16:16:48 +0100

    fixing

--- run_query_spark_submit.sh
+++ run_query_spark_submit.sh
@@ -1,3 +1,4 @@
+SPARK_HOME="/usr/hdp/current/spark-client"
 ALLOWEDIDS=(19 20 21 26 40 52 55 "-A")
 LENGTH=${#ALLOWEDIDS[@]}
 ALL_QUERIES=0
@@ -34,10 +35,9 @@ function executeQuery {
   touch tmp.py
   cat ./queryPreamble.py >> tmp.py
   cat ./queries/query$1.py >> tmp.py
-  ${SPARK_HOME}/spark-submit --master ${MASTER} --executor-memory ${MEMORY_EXECUTOR} --driver-memory ${DRIVER_MEM} --num-executors ${N_EXECUTORS} tmp.py
+  ${SPARK_HOME}/bin/spark-submit --master ${MASTER} --executor-memory ${MEMORY_EXECUTOR} --driver-memory ${DRIVER_MEM} --num-executors ${N_EXECUTORS} tmp.py
 }
 #Entry Point
-./conf.sh
 if [ "$#" -gt 1 ]
 then
   echo "Warning: only one argument is supported, the others will be ignored"
