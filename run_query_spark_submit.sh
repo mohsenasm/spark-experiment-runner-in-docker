eab6ff00ad56893863f3096c0d193b9cde7916df
giorgiopea <annatar93@gmail.com>
2016-12-05 12:54:55 +0100

    continuing log manipulation

--- run_query_spark_submit.sh
+++ run_query_spark_submit.sh
@@ -57,8 +57,9 @@ function executeQuery {
   touch tmp.py
   cat ./queryPreamble.py >> tmp.py
   cat ./queries/query$1.py >> tmp.py
-  APP_ID=$(${SPARK_HOME}/bin/spark-submit --master ${MASTER} --deploy-mode ${DEPLOY} --executor-memory ${MEMORY_EXECUTOR} --driver-memory ${DRIVER_MEM} --num-executors ${N_EXECUTORS} --executor-cores ${EXECUTOR_CORES} tmp.py | grep -m1 "/application_([0-9])+_([0-9])+/")
-  echo ${APP_ID}
+  ${SPARK_HOME}/bin/spark-submit --master ${MASTER} --deploy-mode ${DEPLOY} --executor-memory ${MEMORY_EXECUTOR} --driver-memory ${DRIVER_MEM} --num-executors ${N_EXECUTORS} --executor-cores ${EXECUTOR_CORES} tmp.py &> app_id.txt
+  APP_ID = $(cat app_id.txt | grep -m 1 -Po "application_([0-9]+)_([0-9]+)"))
+  rm app_id.txt
   echo "EXECUTION FINISHED"
   echo "DOWNLOADING LOGS"
   ./logDownload.sh APP_ID
