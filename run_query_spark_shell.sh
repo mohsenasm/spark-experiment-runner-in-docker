0296d68e3289dbbec0ca75b04a9c737d7972a208
giorgiopea <annatar93@gmail.com>
2016-12-10 12:13:53 +0100

    chaning sh denomination and documenting code

--- run_query_spark_shell.sh
+++ run_query_spark_shell.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 ## Copyright 2016 Giorgio Pea <giorgio.pea@mail.polimi.it>
 ##
 ## Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +14,7 @@
 ## See the License for the specific language governing permissions and
 ## limitations under the License.
 
-source ./config.sh
+. config.sh
 LENGTH=${#ALLOWEDIDS[@]}
 ALL_QUERIES=0
 REPETITIONS_N=1
@@ -20,12 +22,15 @@ APP_ID=""
 ENDPOINT=""
 OUTFILE=""
 
+## Checks if the passed parameter is a number, if not exits the process
 function isNumber {
   re='^[0-9]+$'
   if ! [[ $1 =~ $re ]] ; then
      echo "Error: Scale factor in config file or Repetition factor is not an integer number" >&2; exit 1
   fi
 }
+## Checks if the passed parameter exists in ALLOWEDIDS, sets some variables
+## To perform the execution of all queries or of a sigle query multiple time
 function checkargs {
   RESULT=0
   for i in "${ALLOWEDIDS[@]}"
@@ -51,24 +56,31 @@ function checkargs {
     fi
   fi
 }
+## Executes a query with the given id
 function executeQuery {
   if [ -f ./tmp.py ]
   then
       rm tmp.py
   fi
   touch tmp.py
+  ## Generates a file that puts together the query preamble + the query body
   cat ./queryPreamble.py >> tmp.py
   cat ./queries/query$1.py >> tmp.py
+  ## Executes pyspark with stdout/stderr redirect to app_id.txt
   ${PYSPARK}/pyspark tmp.py --deploy-mode ${DEPLOY} --executor-memory ${MEMORY_EXECUTOR} --driver-memory ${DRIVER_MEM} --master ${MASTER} --num-executors ${N_EXECUTORS} --executor-cores ${EXECUTOR_CORES} &> app_id.txt
+  ## Grabs the spark job application id from the redirected stdout/stderr
   APP_ID=$(cat app_id.txt | grep -m 1 -Po "application_([0-9])+_([0-9])")
   mv app_id.txt spark_outputs/${APP_ID}.txt
   echo "EXECUTION FINISHED"
   echo "APP ID: ${APP_ID}"
   sleep ${WAIT_LOG}
   echo "DOWNLOADING LOGS"
+  ## Executes log dowload
   ./logDownload.sh -s ${HISTORY_SERVER_IP} ${APP_ID}
   echo "DOWNLOAD COMPLETED"
 }
+## Entry Point
+## Correct usage check
 if [ $# -eq 0 ]
 then
   echo "Error: usage is [QUERY_ID | -A FOR EXECUTE ALL] ?[N_TIMES]"
@@ -79,6 +91,8 @@ then
   echo "Warning: only one argument is supported, the others will be ignored"
 fi
 checkargs $1 $2
+##
+## Queries execution
 if [ $ALL_QUERIES -eq 1 ]
 then
   for i in $(seq 0 $(expr $LENGTH - 2))
--- run_query_spark_submit.sh
+++ run_query_spark_submit.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 ## Copyright 2016 Giorgio Pea <giorgio.pea@mail.polimi.it>
 ##
 ## Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,20 +14,21 @@
 ## See the License for the specific language governing permissions and
 ## limitations under the License.
 
-source ./config.sh
+. config.sh
 LENGTH=${#ALLOWEDIDS[@]}
 ALL_QUERIES=0
 REPETITIONS_N=1
 APP_ID=""
-ENDPOINT=""
-OUTFILE=""
 
+## Checks if the passed parameter is a number, if not exits the process
 function isNumber {
   re='^[0-9]+$'
   if ! [[ $1 =~ $re ]] ; then
      echo "Error: Scale factor in config file or Repetition factor is not an integer number" >&2; exit 1
   fi
 }
+## Checks if the passed parameter exists in ALLOWEDIDS, sets some variables
+## To perform the execution of all queries or of a sigle query multiple time
 function checkargs {
   RESULT=0
   for i in "${ALLOWEDIDS[@]}"
@@ -51,25 +54,31 @@ function checkargs {
     fi
   fi
 }
+## Executes a query with the given id
 function executeQuery {
   if [ -f ./tmp.py ]
   then
       rm tmp.py
   fi
   touch tmp.py
+  ## Generates a file that puts together the query preamble + the query body
   cat ./queryPreamble.py >> tmp.py
   cat ./queries/query$1.py >> tmp.py
+  ## Executes spark-submit with stdout/stderr redirect to app_id.txt
   ${SPARK_HOME}/bin/spark-submit --master ${MASTER} --deploy-mode ${DEPLOY} --executor-memory ${MEMORY_EXECUTOR} --driver-memory ${DRIVER_MEM} --num-executors ${N_EXECUTORS} --executor-cores ${EXECUTOR_CORES} tmp.py &> app_id.txt
+  ## Grabs the spark job application id from the redirected stdout/stderr
   APP_ID=$(cat app_id.txt | grep -m 1 -Po "application_([0-9]+)_([0-9]+)")
   mv app_id.txt spark_outputs/${APP_ID}.txt
   echo "EXECUTION FINISHED"
   echo "APP ID: ${APP_ID}"
   sleep ${WAIT_LOG}
   echo "DOWNLOADING LOGS"
+  ## Executes log dowload
   ./logDownload.sh -s ${HISTORY_SERVER_IP} ${APP_ID}
   echo "DOWNLOAD COMPLETED"
 }
-#Entry Point
+## Entry Point
+## Correct usage check
 if [ $# -eq 0 ]
 then
   echo "Error: usage is [QUERY_ID | -A FOR EXECUTE ALL] ?[N_TIMES]"
@@ -80,6 +89,8 @@ then
   echo "Warning: only maximum two arguments is supported, the others will be ignored"
 fi
 checkargs $1 $2
+##
+## Queries execution
 if [ $ALL_QUERIES -eq 1 ]
 then
   for i in $(seq 0 $(expr $LENGTH - 2))
